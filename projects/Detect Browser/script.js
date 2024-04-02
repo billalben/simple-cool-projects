4a7135519d379fabaf9daf0885ec7420d786edc3
Benzazoua <billalben252@gmail.com>
2024-04-02 18:11:48 +0100

    Detect Browser

---
+++ projects/Detect Browser/script.js
@@ -0,0 +1,28 @@
+const userAgent = navigator.userAgent.toLowerCase();
+let browser = "";
+
+switch (true) {
+  case /edg/.test(userAgent):
+    browser = "edge";
+    break;
+  case /firefox|fxios/.test(userAgent):
+    browser = "firefox";
+    break;
+  case /opr\//.test(userAgent):
+    browser = "opera";
+    break;
+  case /chrome|chromium|crios/.test(userAgent):
+    browser = "chrome";
+    break;
+  case /safari/.test(userAgent):
+    browser = "safari";
+    break;
+  default:
+    browser = "other";
+    break;
+}
+
+const logo = document.querySelector(`.logos .${browser}`);
+if (logo) {
+  logo.style.opacity = "1";
+}
