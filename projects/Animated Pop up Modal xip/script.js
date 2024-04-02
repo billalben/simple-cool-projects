115864dbec7d8a703a66e3a3bc70bd17db2f0080
Benzazoua <billalben252@gmail.com>
2024-04-02 14:53:42 +0100

    update

--- projects/Animated Pop up Modal xip/script.js
+++ projects/Animated Pop up Modal xip/script.js
@@ -1,28 +1,30 @@
-const section = document.querySelector("section");
-const hireBtn = section.querySelector("#hireBtn");
-const closeBtn = section.querySelectorAll("#close");
-const textArea = section.querySelector("textarea");
-const submitBtn = section.querySelector(".send");
+const $section = document.querySelector("section");
+const $hireBtn = $section.querySelector("[data-hire-btn]");
+const $closeBtn = $section.querySelectorAll("[data-close]");
+const $textArea = $section.querySelector("textarea");
+const $submitBtn = $section.querySelector("[data-submit]");
 
-hireBtn.addEventListener("click", () => {
-  section.classList.add("show");
-  textArea.focus();
+$hireBtn.addEventListener("click", () => {
+  $section.classList.add("show");
+  $textArea.focus();
 });
 
-closeBtn.forEach((cBtn) => {
-  cBtn.addEventListener("click", () => {
-    section.classList.remove("show");
-    textArea.value = "";
+$closeBtn.forEach((cBtn) => {
+  cBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    $section.classList.remove("show");
+    $textArea.value = "";
   });
 });
 
-submitBtn.addEventListener("click", () => {
-  section.classList.remove("show");
-  textArea.value = "";
+$submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  $section.classList.remove("show");
+  $textArea.value = "";
 });
 
-textArea.addEventListener("input", () => {
-  textArea.value !== ""
-    ? submitBtn.classList.add("active")
-    : submitBtn.classList.remove("active");
+$textArea.addEventListener("input", () => {
+  $textArea.value !== ""
+    ? $submitBtn.classList.add("active")
+    : $submitBtn.classList.remove("active");
 });
