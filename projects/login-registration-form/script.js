c74c81ae722f9aba654735593d3056252f593fbe
Benzazoua <billalben252@gmail.com>
2024-04-16 22:19:23 +0100

    Login registration form

---
+++ projects/login-registration-form/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const wrapper = document.querySelector(".wrapper");
+const signupHeader = document.querySelector(".signup header");
+const loginHeader = document.querySelector(".login header");
+
+loginHeader.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+signupHeader.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
